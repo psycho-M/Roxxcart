cd1db71e482a1f92cf874b870d38b3748fb71098
Mandeep Singh <61588507+psycho-M@users.noreply.github.com>
2021-08-19 14:40:52 +0530

    Newletter update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Footer from './components/Footer/Footer';
 import './css/App.css';
+import { NewsLetter } from './components/NewLetter/NewLetter';
 
 function App() {
     return (
         <div>
-            
+            <NewsLetter />
         </div>
     );
 };
