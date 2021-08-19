1530da58d7aa3c907022ffcb7e3bd38a41ff966b
Mandeep Singh <61588507+psycho-M@users.noreply.github.com>
2021-08-19 12:49:38 +0530

    Footer Update

--- src/components/Footer/LeftSide.jsx
+++ src/components/Footer/LeftSide.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { FaPinterest, FaTwitter, FaFacebookF } from 'react-icons/fa';
+import { AiFillYoutube } from 'react-icons/ai';
+import { FiInstagram } from 'react-icons/fi';
+import { ReactComponent as LogoSvg } from './assets/logo.svg';
 
 export function Link({link}) {
     return (
-            <a rel='noreferrer' target='_blank' href={link.url}>{link.name}</a>
+            <a rel='noreferrer' target='_blank' href={link.url}>{link.name ? link.name : link.icon}</a>
     );
 }
 
-function SocialMediaLinks({link}) {
+function SocialMediaLink({link}) {
     return(
         <section className='social-media-link'>
             <Link link={link} />
@@ -18,51 +22,42 @@ function LeftSide() {
     return (
         <section className='left-side'>
             <section className='logo'>
-                <img 
-                    style={{
-                        display: 'inline-block',
-                        padding: '2px',
-                        margin: '3px'}} 
-                        src='' 
-                        alt='logo' />
-                <h2 style={{
-                        display: 'inline-block',
-                        padding: '2px',
-                        margin: '3px'}
-                    }>RoxxCart</h2>
+                < LogoSvg style={{
+                    padding: '7px',
+                }}/>
             </section>
             <section className="description">
-                "RoxxCart designs clothing for the young, old & everyone in between - but most importantly, for the fashionable"
+                RoxxCart designs clothing for the young, old & everyone in between - but most importantly, for the fashionable
             </section>
             <section className="social-media-links">
-                <SocialMediaLinks 
+                <SocialMediaLink 
                     link={{
                         url: 'https://www.facebook.com/',
-                        name: 'facebook'
+                        icon: < FaFacebookF />
                     }} 
                 />
-                <SocialMediaLinks 
+                <SocialMediaLink 
                     link={{
                         url: 'https://www.instagram.com',
-                        name: 'instagram'
+                        icon: <FiInstagram />
                     }} 
                 />
-                <SocialMediaLinks 
+                <SocialMediaLink 
                     link={{
                         url: 'https://www.pinterest.com',
-                        name: 'pinterest'
+                        icon: <FaPinterest />
                     }} 
                 />
-                <SocialMediaLinks 
+                <SocialMediaLink 
                     link={{
                         url: 'https://www.youtube.com/',
-                        name: 'youtube'
+                        icon: <AiFillYoutube />
                     }} 
                 />
-                <SocialMediaLinks 
+                <SocialMediaLink 
                     link={{
                         url: 'https://www.twitter.com/',
-                        name: 'twitter'
+                        icon: <FaTwitter/>
                     }} 
                 />
             </section>
