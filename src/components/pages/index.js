0f8d60a892f2291251a8e4bb83406eb498e6d33e
Mandeep Singh <61588507+psycho-M@users.noreply.github.com>
2021-08-21 22:29:52 +0530

    Added carousel, but not wrking

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Footer from "../Footer/Footer";
 import  NewsLetter  from "../NewLetter/NewLetter";
-
+import CarouselComponent from '../Carousel/Carousel'
 const Home = () => {
   return (
     <section>
+      <CarouselComponent />
       <NewsLetter />
       <Footer />
     </section>
